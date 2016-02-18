827118219c1b8d92a777d3fc680d509c383784ef
diser <diser@asphaltpanthers.com>
2016-02-18 01:38:20 -0500

    Create solution and add person entity.

---
+++ HelloHttp/Entities/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloHttp.Entities
+{
+    public class Person
+    {
+        public String FirstName;
+        public String LastName;
+        public Int32 Age;
+        public Boolean Married;
+        public DateTime BirthDate;
+    }
+}
