a71ad8c6063bc212c6fa07fa1b9e9671bdfe6dbb
diser <diser@asphaltpanthers.com>
2016-02-24 00:56:02 -0500

    Remove unused references.

--- HelloHttp/PredefinedScenarios/PersonTests.cs
+++ HelloHttp/PredefinedScenarios/PersonTests.cs
@@ -1,10 +1,6 @@
 ﻿using HelloHttp.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloHttp.PredefinedScenarios
 {
